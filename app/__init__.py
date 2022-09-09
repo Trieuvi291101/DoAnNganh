eae3fae03c29bfc96391f2b37af0b44094db9b80
Trieuvi291101 <1951052238vi@ou.edu.vn>
2022-09-09 20:22:45 +0700

    up

--- app/__init__.py
+++ app/__init__.py
@@ -7,13 +7,13 @@ from twilio.rest import Client
 app = Flask(__name__)
 
 app.secret_key = "super key" #akj+fg823762531341=2901r-9sd-7g2f98r3sa8d1-2751849
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:06082001@localhost/phongkhamdb?charset=utf8mb4'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Trieuvi2911@localhost/phongkhamdb?charset=utf8mb4'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 cloudinary.config(
-    cloud_name='suna12846ke',
-    api_key='577991942497348',
-    api_secret='9NlOoMXXLUZZS-ExY9XX4Gpsnes'
+    cloud_name='i-h-c-m',
+    api_key='482771674861723',
+    api_secret='UcuyEfGi4mWdqTG2f1F1e_jfJUE'
 )
 
 #twilio infomation
